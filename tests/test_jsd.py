00e6400e8c08d6037d1eb7fd42b48d10cab9f8b8
TimAdams84 <tim-adams@gmx.net>
2024-05-10 14:53:40 +0200

    Change JSD computation base to log_2 to actaully have it bound to 0-1 & automatically map different dtypes to real dtype

--- tests/test_jsd.py
+++ tests/test_jsd.py
@@ -1,20 +1,74 @@
-import os
 from unittest import TestCase
 
 import pandas as pd
 
 import syndat
 
-from syndat.domain import AggregationMethod
-
-
 class Test(TestCase):
-    TEST_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-    real = pd.read_csv(os.path.join(TEST_DIR_PATH, "resources", 'real.csv'))
-    synthetic = pd.read_csv(os.path.join(TEST_DIR_PATH, "resources", 'synthetic.csv'))
+    def test_jsd_zero_int64(self):
+        synthetic = pd.DataFrame({
+            'feature1': [1, 2, 3, 4, 5],
+            'feature2': [10, 11, 12, 13, 14]
+        })
+        # Create real data
+        real = pd.DataFrame({
+            'feature1': [6, 7, 8, 9, 10],
+            'feature2': [15, 16, 17, 18, 19]
+        })
+        jsd = syndat.quality.jsd(real, synthetic)
+        self.assertEqual(0, jsd)
 
-    def test_get_jsd_avg(self):
-        jsd_score = syndat.quality.jsd(self.real, self.synthetic, score=False,
-                                       aggregation_method=AggregationMethod.AVERAGE)
-        self.assertEqual(0.1602252096223203, jsd_score)
+    def test_jsd_zero_int64_float64(self):
+        synthetic = pd.DataFrame({
+            'feature1': [1, 2, 3, 4, 5],
+            'feature2': [0.1, 0.2, 0.3, 0.4, 0.5]
+        })
+        # Create real data
+        real = pd.DataFrame({
+            'feature1': [6, 7, 8, 9, 10],
+            'feature2': [0.6, 0.7, 0.8, 0.9, 1.0]
+        })
+        jsd = syndat.quality.jsd(real, synthetic)
+        self.assertEqual(jsd, 0)
+
+    def test_jsd_perfect_int64(self):
+        synthetic = pd.DataFrame({
+            'feature1': [1, 2, 1, 2, 3],
+            'feature2': [11, 12, 13, 14, 15]
+        })
+        # Create real data
+        real = pd.DataFrame({
+            'feature1': [1, 2, 1, 2, 3],
+            'feature2': [11, 12, 13, 14, 15]
+        })
+        jsd = syndat.quality.jsd(real, synthetic)
+        self.assertEqual(jsd, 100)
+
+    def test_jsd_perfect_int64_and_float64(self):
+        synthetic = pd.DataFrame({
+            'feature1': [1, 2, 1, 2, 3],
+            'feature2': [0.1, 0.2, 0.3, 0.4, 0.5]
+        })
+        # Create real data
+        real = pd.DataFrame({
+            'feature1': [1, 2, 1, 2, 3],
+            'feature2': [0.1, 0.2, 0.3, 0.4, 0.5]
+        })
+        jsd = syndat.quality.jsd(real, synthetic)
+        self.assertEqual(jsd, 100)
+
+    def test_jsd_different_coltypes(self):
+        synthetic = pd.DataFrame({
+            'feature1': [1, 2, 1, 2, 3],
+            'feature2': [1.1, 2.1, 3.1, 4.1, 5.1]
+        })
+        # Create real data
+        real = pd.DataFrame({
+            'feature1': [1.2, 2.1, 1.1, 2.1, 3.1],
+            'feature2': [1, 2, 3, 4, 5]
+        })
+        jsd = syndat.quality.jsd(real, synthetic, score=False)
+        print(jsd)
+        print(jsd)
+
