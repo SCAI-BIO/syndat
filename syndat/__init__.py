5b33fab1d4f45268d2970b9b28b2e01e896788fa
Tim Adams <tim.adams@scai.fraunhofer.de>
2024-09-27 16:46:04 +0200

    chore: improve init file

--- syndat/__init__.py
+++ syndat/__init__.py
@@ -1,3 +1,40 @@
-from syndat import domain
-from syndat import scores
-from syndat import visualization
+from .visualization import (
+    plot_distributions,
+    plot_correlations,
+    plot_shap_discrimination,
+    plot_categorical_feature,
+    plot_numerical_feature
+)
+
+from .scores import (
+    auc,
+    jsd,
+    discrimination,
+    distribution,
+    correlation,
+)
+
+from .postprocessing import (
+    normalize_scale,
+    normalize_float_precision,
+    assert_minmax,
+) 
+
+__all__ = [
+    # visualization
+    'plot_distributions',
+    'plot_correlations',
+    'plot_shap_discrimination',
+    'plot_categorical_feature',
+    'plot_numerical_feature',
+    # scores
+    'auc',
+    'jsd',
+    'discrimination',
+    'distribution',
+    'correlation',
+    # postprocessing
+    'normalize_scale',
+    'normalize_float_precision',
+    'assert_minmax',
+]
