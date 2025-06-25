dfc750fbd7eeb608c4ae6534882fd2ca8e30a1a2
Tim Adams <tim.adams@scai.fraunhofer.de>
2025-06-25 13:39:09 +0200

    chore: update Sphinx dependencies and configuration for documentation build

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,15 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../src'))
+project = 'syndat'
+copyright = '2025, Fraunhofer SCAI'
+author = 'Tim Adams'
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx_autodoc_typehints'
+    "sphinx.ext.autodoc",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.napoleon",
 ]
 
-html_theme = 'sphinx_rtd_theme'
+templates_path = ['_templates']
+exclude_patterns = []
 
+html_theme = "sphinx_rtd_theme"
+#html_static_path = ['_static']
