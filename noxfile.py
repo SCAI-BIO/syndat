a564760bbbd38ca2d18b97b093a1879ff1e63795
Tim Adams <tim-adams@gmx.net>
2026-04-01 11:52:39 +0200

    chore: drop python 3.9 support

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@ import nox
 nox.options.default_venv_backend = "uv"
 nox.options.sessions = ["tests"]
 
-PYTHON_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13"]
+PYTHON_VERSIONS = ["3.10", "3.11", "3.12", "3.13"]
 
 
 @nox.session(python=PYTHON_VERSIONS)
